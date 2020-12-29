e59de3a7b2717f3b7001d49f0f19fe1ebc3b97d6
AlfredWibowo <75579330+AlfredWibowo@users.noreply.github.com>
2020-12-29 22:59:14 +0700

    pion shield images

--- Definition.h
+++ Definition.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#define MAP_BG "image/map2.png"
+#define MAP "image/map2.png"
+
 #define ROLL_BUTTON "image/roll button.png"
 #define PLAY_BUTTON "image/play button.png"
 #define ABOUT_BUTTON "image/about button.png"
@@ -9,23 +10,32 @@
 #define NEXT_BUTTON "image/next button.png"
 #define PREV_BUTTON "image/prev button.png"
 #define QUIT_BUTTON "image/quit button.png"
+
 #define DICE_ONE "image/one.png"
 #define DICE_TWO "image/two.png"
 #define DICE_THREE "image/three.png"
 #define DICE_FOUR "image/four.png"
 #define DICE_FIVE "image/five.png"
 #define DICE_SIX "image/six.png"
+
 #define PION_HIJAU "image/hijau.png"
 #define PION_MERAH "image/merah.png"
 #define PION_KUNING "image/kuning.png"
 #define PION_BIRU "image/biru.png"
+#define PION_HIJAU_SHIELD "image/hijau_shield.png"
+#define PION_MERAH_SHIELD "image/merah_shield.png"
+#define PION_KUNING_SHIELD "image/kuning_shield.png"
+#define PION_BIRU_SHIELD "image/biru_shield.png"
+
 #define MAIN_MENU_BG "image/BG_A.jpg"
 #define ABOUT_BG "image/about bg.png"
 #define TUTORIAL1_BG "image/tutorial1 bg.png"
 #define TUTORIAL2_BG "image/tutorial2 bg.png"
 #define GAME_BG "image/game bg.png"
+
 #define FONT "font/Embryotic.ttf"
 #define FONT2 "font/SeasideResortNF.ttf"
+
 #define MUSIC "music/ludo_music.wav"
 
 #define WIDTH 1100
